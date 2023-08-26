c58255fcf86242cf09b7ba38350ba84ecf81db87
DaniAngione <daniangi@gmail.com>
2023-08-26 21:32:22 +0200

    Update hunter_loot_drops_component.lua

--- monkey_patches/hunter_loot_drops_component.lua
+++ monkey_patches/hunter_loot_drops_component.lua
@@ -1,5 +1,6 @@
 local LootTable = require 'stonehearth.lib.loot_table.loot_table'
 local LootDropsComponent = require 'stonehearth.components.loot_drops.loot_drops_component'
+local log = radiant.log.create_logger('loot_drops_component')
 
 -- ACE Compatibility
 local AceLootDropsComponent = require 'stonehearth_ace.monkey_patches.ace_loot_drops_component'
@@ -48,22 +49,23 @@ function HunterLootDropsComponent:_on_kill_event(e)
       end
    
       if loot_table then
-         local location = radiant.entities.get_world_grid_location(self._entity)
+         local kill_data = e.kill_data or {}
+         local location = kill_data.location or radiant.entities.get_world_grid_location(self._entity)
          if location then
             local player_id = radiant.entities.get_player_id(self._entity)
-            local kill_data = e.kill_data or {}
-            local loot_player_id = kill_data.source or self._sv.auto_loot_player_id
+            local loot_player_id = kill_data.source or kill_data.source_id or self._sv.auto_loot_player_id
             if radiant.entities.is_entity(loot_player_id) then
                loot_player_id = radiant.entities.get_player_id(loot_player_id)
             end
             local auto_loot, force_auto_loot
-            if self._sv.auto_loot_player_id then
-               auto_loot = stonehearth.client_state:get_client_gameplay_setting(self._sv.auto_loot_player_id, 'stonehearth', 'auto_loot', false)
-               force_auto_loot = loot_table.force_auto_loot or player_id == self._sv.auto_loot_player_id
+            if loot_player_id then
+               auto_loot = stonehearth.client_state:get_client_gameplay_setting(loot_player_id, 'stonehearth', 'auto_loot', false)
+               force_auto_loot = loot_table.force_auto_loot or player_id == loot_player_id
             elseif loot_player_id == player_id then
                force_auto_loot = true
             end
-            local town = stonehearth.town:get_town(self._sv.auto_loot_player_id)
+            log:debug('%s dropping loot: %s, %s, %s, %s', self._entity, player_id, tostring(loot_player_id), tostring(auto_loot), tostring(force_auto_loot))
+            local town = stonehearth.town:get_town(loot_player_id)
    
             local quality
             if loot_table.apply_entity_quality then
@@ -82,24 +84,30 @@ function HunterLootDropsComponent:_on_kill_event(e)
                output = self._entity,
                spill_fail_items = true,
             }
-            local spawned_entities = radiant.entities.output_items(items, location, 1, 3, options).spilled
+            local min_distance = loot_table.drop_range and loot_table.drop_range.min or 1
+            local max_distance = loot_table.drop_range and loot_table.drop_range.max or 3
+            local spawned_entities = radiant.entities.output_items(items, location, min_distance, max_distance, options).spilled
    
             --Add a loot command to each of the spawned items, or claim them automatically
-            for id, entity in pairs(spawned_entities) do
-               local target = entity
-               local entity_forms = entity:get_component('stonehearth:entity_forms')
-               if entity_forms then
-                  target = entity_forms:get_iconic_entity()
-               end
-               if not force_auto_loot then
-                  local command_component = target:add_component('stonehearth:commands')
-                  command_component:add_command('stonehearth:commands:loot_item')
-                  if auto_loot and town then
-                     town:loot_item(target)
+            if not force_auto_loot then
+               for id, entity in pairs(spawned_entities) do
+                  local target = entity
+                  local entity_forms = entity:get_component('stonehearth:entity_forms')
+                  if entity_forms then
+                     target = entity_forms:get_iconic_entity() or target
+                  end
+   
+                  -- make sure the item is in the world and hasn't been scooped up by something
+                  if radiant.entities.get_world_grid_location(target) then
+                     local command_component = target:add_component('stonehearth:commands')
+                     command_component:add_command('stonehearth:commands:loot_item')
+                     if auto_loot and town then
+                        town:loot_item(target)
+                     end
                   end
                end
             end
-            
+
             if self._sv._hunting_camp_loot then 
                local destination = self._sv._hunting_camp_loot:get_component('stonehearth:storage')
                for id, entity in pairs(spawned_entities) do
